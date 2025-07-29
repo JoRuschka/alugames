b0cfa8d67884c7f7057ed76ac5f46b9a187847b8
JoRuschka <148093751+JoRuschka@users.noreply.github.com>
2025-07-28 22:21:37 -0300

    Atualizando app.js

--- app.js
+++ app.js
@@ -1,19 +1,38 @@
+let jogosAlugados = 0;
+
+function contarJogosAlugados() {
+    console.log(`Total de jogos alugados: ${jogosAlugados}`);
+}
+
 function alterarStatus(id) {
     let gameSelect = document.getElementById(`game-${id}`);
     let imagem = gameSelect.querySelector('.dashboard__item__img');
-/*    let name = gameSelect.querySelector('.dashboard__item__name'); */
+    let name = gameSelect.querySelector('.dashboard__item__name');
     let botao = gameSelect.querySelector('.dashboard__item__button');
 
     if (imagem.classList.contains('dashboard__item__img--rented')) {
-        imagem.classList.remove('dashboard__item__img--rented');
-        botao.textContent = 'Alugar';
-        botao.classList.remove('dashboard__item__button--return');
+        // Adicionando confirmação de devolução do jogo
+        if (confirm(`Você tem certeza que deseja devolver o jogo ${name.textContent}?`)) {
+            imagem.classList.remove('dashboard__item__img--rented');
+            botao.textContent = 'Alugar';
+            botao.classList.remove('dashboard__item__button--return');
+            jogosAlugados--;
+        }        
     } else {
         imagem.classList.add('dashboard__item__img--rented');
         botao.textContent = 'Devolver';
         botao.classList.add('dashboard__item__button--return');
-    }   
+        jogosAlugados++;
+    }
+    contarJogosAlugados();
 }
+
+//Contagem de jogos alugados
+document.addEventListener('DOMContentLoaded', function() {
+    jogosAlugados = document.querySelectorAll('.dashboard__item__img--rented').length;
+    contarJogosAlugados();
+});
+
 
 /* A classe do html que indica se o jogo está alugado é: "dashboard__item__img--rented"
    O jogo que não tem essa classe, indica que está disponível.
@@ -28,4 +47,7 @@ function alterarStatus(id) {
    Para alterar o texto do botão (botao), atribuímos o valor, utilizando textContent.
    Para alterar a cor do botão Alugar/Devolver, verifica-se a classe no html:
    "dashboard__item__button--return", e remove ou adiciona as características definidas para o botão.
+
+   O evento DOMContentLoaded conta a quantidade inicial de jogos alugados quando a página é carregada.
+   Nesse momento ele conta a quantidade inicial de jogos alugados.
 */
